af632bba2969e5fc4cb0b2c6210b59fe77112191
RotDr <rotariudragos36@gmail.com>
2026-09-02 11:04:33 +0300

    hotfix

--- DataBase/db_schema.py
+++ DataBase/db_schema.py
@@ -15,7 +15,7 @@ def create_database_schema(con):
         description TEXT,
         deadline TIMESTAMP,
         importance INTEGER,
-        FOREIGN KEY(project_id) REFERENCES project(id) ON DELETE CASCADE);
+        FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE CASCADE);
 
     CREATE TABLE IF NOT EXISTS documents(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -30,8 +30,8 @@ def create_database_schema(con):
         text TEXT,
         chapter TEXT,
         subchapter TEXT,
-        FOREIGN KEY(obligation_id) REFERENCES obligation(id) ON DELETE CASCADE,
-        FOREIGN KEY(document_id) REFERENCES document(id) ON DELETE CASCADE);
+        FOREIGN KEY(obligation_id) REFERENCES obligations(id) ON DELETE CASCADE,
+        FOREIGN KEY(document_id) REFERENCES documents(id) ON DELETE CASCADE);
     """)
 
 def setup_database():
